ddc8182cd230ccaee13f384a729e2b6d78424755
root <root@server.sebastian-christoph.de>
2019-06-16 01:03:44 +0200

    addded functionality

--- Classes/Controller/UserlistController.php
+++ Classes/Controller/UserlistController.php
@@ -8,6 +8,24 @@ namespace SebastianChristoph\ScFeuserlist\Controller;
  */
 class UserlistController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
+
+    /**
+     * feRepository
+     *
+     * @var \TYPO3\CMS\Extbase\Domain\Repository\FrontendUserRepository
+     * @inject
+     */
+    protected $feRepository;
+
+    /**
+     * @param FeRepository $feRepository
+     */
+    public function injectFeRepository(FeRepository $feRepository)
+    {
+        $this->feRepository = $feRepository;
+    }
+
+
     /**
      * list action
      *
@@ -15,7 +33,7 @@ class UserlistController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
      */
     public function listAction()
     {
-        $list = 'alleuser';
-        $this->view->assign('list', $list);
+        $allUsers = $this->feRepository->findAll();
+        $this->view->assign('users', $allUsers);
     }
 }

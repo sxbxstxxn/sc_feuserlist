f4eba8a51909cdad429d0f9c558c6f3f42bc7472
root <root@server.sebastian-christoph.de>
2019-06-15 17:48:48 +0200

    new initial

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,48 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'sc_feuserlist.sc_feuserlist',
-    'Pi1',
-    [
-        'sc_feuserlist' => 'list',
-    ],
-    // non-cacheable actions
-    [
-        'sc_feuserlist' => 'list',
-    ]
+call_user_func(
+    function()
+    {
+
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'Scvendor.Scextensionkey',
+            'Pluginkey',
+            [
+                
+            ],
+            // non-cacheable actions
+            [
+                
+            ]
+        );
+
+    // wizards
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        'mod {
+            wizards.newContentElement.wizardItems.plugins {
+                elements {
+                    pluginkey {
+                        iconIdentifier = scextensionkey-plugin-pluginkey
+                        title = LLL:EXT:scextensionkey/Resources/Private/Language/locallang_db.xlf:tx_scextensionkey_pluginkey.name
+                        description = LLL:EXT:scextensionkey/Resources/Private/Language/locallang_db.xlf:tx_scextensionkey_pluginkey.description
+                        tt_content_defValues {
+                            CType = list
+                            list_type = scextensionkey_pluginkey
+                        }
+                    }
+                }
+                show = *
+            }
+       }'
+    );
+		$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+		
+			$iconRegistry->registerIcon(
+				'scextensionkey-plugin-pluginkey',
+				\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+				['source' => 'EXT:scextensionkey/Resources/Public/Icons/user_plugin_pluginkey.svg']
+			);
+		
+    }
 );
